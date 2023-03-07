2503e3af3e624421b7c200fb52756a4e0f599153
ando-zone <dev.dohyeonan@gmail.com>
2023-03-08 01:46:48 +0900

    chore: 초기 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -11,24 +11,7 @@ import {
 } from "@chakra-ui/react"
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <VStack spacing={8}>
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
+  <div>
+    <Text color={"gray.500"} fontSize={"6xl"}>It works!!!!</Text>
+  </div>
 )
