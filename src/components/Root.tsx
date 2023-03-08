bc2141efe38dacf811d5c08726e25e66dc1806ff
ando-zone <dev.dohyeonan@gmail.com>
2023-03-09 00:10:03 +0900

    feat: router setup

---
+++ src/components/Root.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+export default function Root(){
+    return (
+        <h1>
+            im root
+            {/* Outlet이 하는 일은 루트의 중앙에 우리가 렌더링 하고싶은 컴포넌트를 위치시키는 것이다. */}
+            {/* Outlet이 현재의 URL과 일치하는 자식을 렌더링한다. */}
+            <Outlet />
+        </h1>
+    )
+}
